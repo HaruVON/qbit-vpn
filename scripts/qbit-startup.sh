10d5853ddafc00079142e6956817ba3c78d4449b
haruvon <tech@devinswift.dev>
2023-08-30 15:58:51 -0700

    fix: check protonvpn location

--- scripts/qbit-startup.sh
+++ scripts/qbit-startup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 /scripts/default-script.exp
-printf '5\n2\n' | /root/.local/bin/protonvpn configure
+#printf '5\n2\n' | /root/.local/bin/protonvpn configure
 #/root/.local/bin/protonvpn c --p2p
 exec /init
