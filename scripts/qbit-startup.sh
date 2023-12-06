72f40be486ce36d4705a1e43934eb17c0c60cfe9
haruvon <tech@devinswift.dev>
2023-12-05 21:33:17 -0800

    fix: check to see if there is a pvpn config before running init

--- scripts/qbit-startup.sh
+++ scripts/qbit-startup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-/scripts/default-script.exp
+if ! [ -e /root/.pvpn-cli/pvpn-cli.cfg ]
+then
+    /scripts/default-script.exp
+fi
+
 printf '5\n2\n' | /root/.local/bin/protonvpn configure
 /root/.local/bin/protonvpn c --p2p
 exec /init
