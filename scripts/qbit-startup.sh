e710ab0b2361f6661c59b4847fcacf4ab2113ee5
Devin Swift <haru@devinswift.dev>
2023-12-05 21:42:03 -0800

    fix: revert qbit-startup changes

--- scripts/qbit-startup.sh
+++ scripts/qbit-startup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if ! [ -e /root/.pvpn-cli/pvpn-cli.cfg ]
-then
-    /scripts/default-script.exp
-fi
+#if ! [ -e /root/.pvpn-cli/pvpn-cli.cfg ]
+#then
+/scripts/default-script.exp
+#fi
 
 printf '5\n2\n' | /root/.local/bin/protonvpn configure
 /root/.local/bin/protonvpn c --p2p
